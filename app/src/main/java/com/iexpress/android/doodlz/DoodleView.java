89b75fb617999965a097c15c3581a9bca84eb51b
jwang1 <sigkdd@yahoo.com>
2018-09-19 23:02:28 -0400

    1. UI side   1.1 Added a view -- DoodleView   1.2 Added fragment for color chooser -- where textview as label and seekerBar for color value selection

---
+++ app/src/main/java/com/iexpress/android/doodlz/DoodleView.java
@@ -0,0 +1,10 @@
+package com.iexpress.android.doodlz;
+
+import android.content.Context;
+import android.view.View;
+
+public class DoodleView extends View {
+    public DoodleView(Context context) {
+        super(context);
+    }
+}
